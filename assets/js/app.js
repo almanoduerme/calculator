9b1f55a5255ee2560515362e062a4bc3590efdac
Nahuel Gómez <ngomezpf@gmail.com>
2022-01-12 07:53:31 -0300

    Se simplificó el código

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,12 @@
 // GLOBAL VARIABLES
 
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-const four = document.querySelector(".four");
-const five = document.querySelector(".five");
-const six = document.querySelector(".six");
-const seven = document.querySelector(".seven");
-const eight = document.querySelector(".eight");
-const nine = document.querySelector(".nine");
-const zero = document.querySelector(".zero");
-const plus = document.querySelector(".plus");
-const minus = document.querySelector(".minus");
-const multiply = document.querySelector(".multiply");
-const divide = document.querySelector(".divide");
 const equals = document.querySelector(".equals");
 const clear = document.querySelector(".clear");
-const decimal = document.querySelector(".decimal");
 const display = document.querySelector(".display-text-value");
-const btn = document.querySelectorAll(".btn");
 
 // ARRAY
 
-const buttons = [one, two, three, four, five, six, seven, eight, nine, zero, plus, minus, multiply, divide, decimal];
+let buttons = Array.from(document.querySelectorAll('.btn'));
 
 // BUTTONS FUNCTIONS
 
@@ -31,10 +15,8 @@ buttons.forEach(function (item) {
     if (display.textContent === "0") {
       display.textContent = "";
       display.textContent += item.textContent;
-      console.log(display.textContent);
     } else {
       display.textContent += item.textContent;
-      console.log(display.textContent);
     }
   });
 });
