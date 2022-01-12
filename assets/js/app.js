3d971fd19287e609e4d60610443e80027377150f
Nahuel Gómez <ngomezpf@gmail.com>
2022-01-12 07:31:00 -0300

    Se simplificó el código

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,159 +20,27 @@ const decimal = document.querySelector(".decimal");
 const display = document.querySelector(".display-text-value");
 const btn = document.querySelectorAll(".btn");
 
-// EVENT LISTENERS
+// ARRAY
 
-/*
-btn.forEach((btn) => {
-  btn.addEventListener("click", function () {
+const buttons = [
+  one, two, three, four, five, six, seven, eight, nine, zero, plus, minus, multiply, divide, decimal];
+
+// BUTTONS FUNCTIONS
+
+buttons.forEach(function (item) {
+  item.addEventListener("click", function () {
     if (display.textContent === "0") {
       display.textContent = "";
-      display.textContent += btn.textContent;
+      display.textContent += item.textContent;
       console.log(display.textContent);
     } else {
-      display.textContent += btn.textContent;
+      display.textContent += item.textContent;
       console.log(display.textContent);
     }
   });
 });
-*/
 
-zero.addEventListener("click", function () {
-  if (display.textContent === "0") {
-    display.textContent = "";
-    display.textContent += zero.textContent;
-  } else {
-    display.textContent += zero.textContent;
-  }
-});
-
-one.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += 1;
-  } else {
-    display.textContent += 1;
-  }
-});
-
-two.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += 2;
-  } else {
-    display.textContent += 2;
-  }
-});
-
-three.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += 3;
-  } else {
-    display.textContent += 3;
-  }
-});
-
-four.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += 4;
-  } else {
-    display.textContent += 4;
-  }
-});
-
-five.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += 5;
-  } else {
-    display.textContent += 5;
-  }
-});
-
-six.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += 6;
-  } else {
-    display.textContent += 6;
-  }
-});
-
-seven.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += 7;
-  } else {
-    display.textContent += 7;
-  }
-});
-
-eight.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += 8;
-  } else {
-    display.textContent += 8;
-  }
-});
-
-nine.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += 9;
-  } else {
-    display.textContent += 9;
-  }
-});
-
-// OPERADORES
-
-divide.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += "/";
-  } else {
-    display.textContent += "/";
-  }
-});
-
-multiply.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += "*";
-  } else {
-    display.textContent += "*";
-  }
-});
-
-minus.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += "-";
-  } else {
-    display.textContent += "-";
-  }
-});
-
-plus.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += "+";
-  } else {
-    display.textContent += "+";
-  }
-});
-
-decimal.addEventListener("click", function () {
-  if (display.textContent == 0) {
-    display.textContent = "";
-    display.textContent += ".";
-  } else {
-    display.textContent += ".";
-  }
-});
+// EVENT LISTENERS
 
 clear.addEventListener("click", function () {
   display.textContent = "0";
@@ -180,4 +48,13 @@ clear.addEventListener("click", function () {
 
 equals.addEventListener("click", function () {
   display.textContent = eval(display.textContent);
+  maxLength(display);
 });
+
+// FUNCTIONS
+
+const maxLength = function (display) {
+  if (display.textContent.length > 9) {
+    display.textContent = "Error";
+  }
+};
